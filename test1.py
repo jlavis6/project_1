0df8df43c892ea20706268203c74725b00a4c8fd
jlavis6 <jlavis6@gmail.com>
2020-05-22 23:22:27 -0500

    second commit

--- test1.py
+++ test1.py
@@ -1,1 +1,10 @@
-print("hello")
+import os
+import pandas as pd
+
+#os.chdir("Users/jlavi/OneDrive - IPADE Business School/Coding")
+
+print(os.getcwd())
+print()
+df = pd.read_excel("Bank Accounts.xlsx", sheet_name="2Now")
+
+print(df)
