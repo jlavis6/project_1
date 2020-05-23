7860e9965821732d402ae005241a15d7210113a1
jlavis6 <jlavis6@gmail.com>
2020-05-23 11:16:56 -0500

    fourth commit

--- test1.py
+++ test1.py
@@ -1,3 +1,13 @@
 import camelot
+import os
 
-pdf = camelot.read_pdf('To invest or not to.pdf')
+BASE_PATH = "C:\\Users\jlavi\OneDrive - IPADE Business School\\"
+
+a = os.path.join(BASE_PATH, "To invest or not to.pdf")
+
+b = os.path.isfile(a)
+
+print(b)
+print()
+
+#pdf = camelot.read_pdf('To invest or not to.pdf')
